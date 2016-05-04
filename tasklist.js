b5ca7c74357b3dcefa76edc055a65393a8c9ff86
Tristan Gruener <tristan.gruener@gmail.com>
2016-05-04 11:36:07 -0700

    initial commit

---
+++ tasklist.js
@@ -0,0 +1,45 @@
+TaskList = new Mongo.Collection('tasks');
+
+if(Meteor.isClient){
+
+	Template.todolist.helpers({
+		'task' : function(){
+			return TaskList.find();
+		},
+		'currentTask' : function(){
+			
+			var taskId = this._id;
+			var currentTask = Session.get('selectedTask');
+
+			if(taskId == currentTask){
+				return "selected";
+			}
+		}
+	});
+
+	Template.todolist.events({
+		'click .btns' : function(){
+			TaskList.remove(this._id);
+		}
+	});
+
+	Template.addTaskForm.events({
+		'submit form' : function(){
+			event.preventDefault();
+			var taskVar = event.target.newTask.value;
+			if(taskVar == ''){
+				alert("try writing something first...");
+			}else{
+				TaskList.insert({
+					desc: taskVar
+				});
+			}
+			event.target.newTask.value = '';
+		}
+	});
+}
+
+if(Meteor.isServer){
+	
+}
+
